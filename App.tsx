c3131b6c9a53dc23dc9746c5072f681aa8a25a8f
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-07 20:55:35 +0300

    added header color

--- App.tsx
+++ App.tsx
@@ -1,17 +1,29 @@
 import React from "react";
+import { Platform } from "react-native";
 import { enableScreens } from "react-native-screens";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import Colors from "./constants/Colors";
 import reducers from "./redux";
 import ProductsOverviewScreen from "./screens/ProductsOverviewScreen";
 enableScreens();
 const store = createStore(reducers);
 
-const stackNavigator = createStackNavigator({
-  Products: ProductsOverviewScreen
-});
+const stackNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        ...(Platform.OS === "android" && { backgroundColor: Colors.primary })
+      },
+      headerTintColor: Platform.OS ? "white" : Colors.primary
+    }
+  }
+);
 
 const App = createAppContainer(stackNavigator);
 

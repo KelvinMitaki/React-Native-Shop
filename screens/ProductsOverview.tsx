941eb395b529b26e2bd55bf7ec2b4c9d5e69de9a
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-07 20:20:16 +0300

    scaffolded screens

---
+++ screens/ProductsOverview.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ProductsOverview = () => {
+  return (
+    <View>
+      <Text>ProductsOverview</Text>
+    </View>
+  );
+};
+
+export default ProductsOverview;
+
+const styles = StyleSheet.create({});
---
+++ screens/UserProductsScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const UserProductsScreen = () => {
+  return (
+    <View>
+      <Text>UserProductsScreen</Text>
+    </View>
+  );
+};
+
+export default UserProductsScreen;
+
+const styles = StyleSheet.create({});

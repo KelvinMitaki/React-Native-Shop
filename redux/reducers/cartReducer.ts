44d4beeadb4d5ad9e8e0b684c919ec85863ccc53
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-09 17:43:36 +0300

    fixed add to cart bug

--- redux/reducers/cartReducer.ts
+++ redux/reducers/cartReducer.ts
@@ -25,21 +25,24 @@ const INITIAL_STATE: CartState = {
 const cartReducer = (state = INITIAL_STATE, action: Action): CartState => {
   switch (action.type) {
     case "addToCart":
-      let items = state.items;
-      let item = items.findIndex(i => i.id === action.payload.id);
-      if (item !== -1) {
-        const quantity = items[item].quantity;
-        items[item] = { ...action.payload, quantity: quantity + 1 };
-      } else {
-        items = [{ ...action.payload, quantity: 1 }, ...items];
+      if (action.payload && Object.keys(action.payload).length !== 0) {
+        let items = state.items;
+        let item = items.findIndex(i => i.id === action.payload.id);
+        if (item !== -1) {
+          const quantity = items[item].quantity;
+          items[item] = { ...action.payload, quantity: quantity + 1 };
+        } else {
+          items = [{ ...action.payload, quantity: 1 }, ...items];
+        }
+        const totalAmount = items
+          .map(it => it.quantity * it.price)
+          .reduce((acc, cur) => acc + cur);
+        const totalQuantity = items
+          .map(it => it.quantity)
+          .reduce((acc, cur) => acc + cur);
+        return { ...state, items, totalAmount, totalQuantity };
       }
-      const totalAmount = items
-        .map(it => it.quantity * it.price)
-        .reduce((acc, cur) => acc + cur);
-      const totalQuantity = items
-        .map(it => it.quantity)
-        .reduce((acc, cur) => acc + cur);
-      return { ...state, items, totalAmount, totalQuantity };
+      return state;
     case "removeFromCart":
       let cartItems = state.items;
       const itemIndex = cartItems.findIndex(it => it.id === action.payload);
